4ece8dd803eea6e2588387d910a70f0d188b88d0
masoom  agrawal <35285250+masoomagrawal1@users.noreply.github.com>
2020-10-15 12:06:44 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,29 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.x=x;
+      this.y=y;
+      this.width = width;
+      this.height = height;
+      this.color=color(random(0,255),random(0,255),random(0,255));
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      //strokeWeight(4);
+      //stroke("green");
+      fill(this.color);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tfff
+# p5.play-boilerplate
+Boiler plate for p5.play
